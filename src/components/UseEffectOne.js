2860a1efed56b779ecfe2b14f30a53814901c29f
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-07-22 23:24:09 +0600

    Conditionally run useEffect in class component

--- src/components/UseEffectOne.js
+++ src/components/UseEffectOne.js
@@ -4,7 +4,8 @@ class UseEffectOne extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            count: 0
+            count: 0,
+            name: ''
         }
     }
 
@@ -14,15 +15,21 @@ class UseEffectOne extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log('Updating document title');
-        document.title = `Clicked ${this.state.count} times`;
+        if(prevState.count !== this.state.count) {
+            console.log('Updating document title');
+            document.title = `Clicked ${this.state.count} times`;
+        }
         console.log('componentDidUpdate');
     }
 
     render() {
         return (
             <div>
-                
+                <input 
+                    type="text" 
+                    value={this.state.name} 
+                    onChange={event => this.setState({name: event.target.value})}
+                />
                 <button 
                     onClick={() => this.setState({count: this.state.count + 1})}
                 >
