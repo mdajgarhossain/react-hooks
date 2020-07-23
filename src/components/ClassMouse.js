aeb2234b8f0b35150239c396ab33c0aa5f04a1e3
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-07-23 06:39:10 +0600

    Run effects only once in class component

---
+++ src/components/ClassMouse.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class ClassMouse extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            x: 0,
+            y: 0
+        }
+    }
+
+    logMousePosition = event => {
+        this.setState({x: event.clientX, y: event.clientY});
+    }
+
+    componentDidMount() {
+        window.addEventListener('mousemove', this.logMousePosition);
+    }
+
+    render() {
+        return (
+            <div>
+                X - {this.state.x} Y - {this.state.y}
+            </div>
+        )
+    }
+}
