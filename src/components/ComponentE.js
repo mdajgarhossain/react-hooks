37d410d595f107ff573e84b3a11dce8bae2c4380
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-08-02 20:07:27 +0600

    useContext Hook Part 3- consume context value in a simplest way

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import ComponentF from './ComponentF'
+import {UserContext, ChannelContext} from '../App';
 
 function ComponentE() {
+    const user = useContext(UserContext);
+    console.log(user);
+    const channel = useContext(ChannelContext);
     return (
         <div>
             <ComponentF />
+            <h4>Another approach to consume context value</h4>
+            <div>
+                UserContext: {user},
+                ChannelContext: {channel}
+            </div>
         </div>
     )
 }
