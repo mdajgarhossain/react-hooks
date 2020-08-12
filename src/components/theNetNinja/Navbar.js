657a9256348397bf997e59fe2e8b0bb8143a4682
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-08-12 08:25:55 +0600

    Context app - part 3 - Access context - 2nd approach

--- src/components/theNetNinja/Navbar.js
+++ src/components/theNetNinja/Navbar.js
@@ -1,22 +1,26 @@
 import React from 'react'
-import { useContext } from 'react'
 import { ThemeContext } from './contexts/ThemeContext';
 
 function Navbar() {
-    const themeState = useContext(ThemeContext);
-    console.log(themeState);
-    const {isLightTheme, light, dark} = themeState;
-    const theme = isLightTheme ? light : dark;
-
     return (
-        <nav style={{background: theme.ui, color: theme.syntax}}>
-            <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-        </nav>
+        <ThemeContext.Consumer>
+            {
+                themeState => {
+                    const {isLightTheme, light, dark} = themeState;
+                    const theme = isLightTheme ? light : dark;
+                    return(
+                        <nav style={{background: theme.ui, color: theme.syntax}}>
+                            <h1>Context App</h1>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    )
+                }
+            }
+        </ThemeContext.Consumer>
     )
 }
 
