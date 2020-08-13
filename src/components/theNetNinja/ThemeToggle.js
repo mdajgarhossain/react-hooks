3f65ced3d43b39b6040caf9f9601aa5b200daaef
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-08-13 20:33:20 +0600

    Context app - part 5 - Consume Multiple Contexts

--- src/components/theNetNinja/ThemeToggle.js
+++ src/components/theNetNinja/ThemeToggle.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useContext } from 'react'
 import { ThemeContext } from './contexts/ThemeContext';
+import { AuthContext } from './contexts/AuthContext';
 
 function ThemeToggle() {
     const {toggleTheme} = useContext(ThemeContext);
+    const {isAuthenticated} = useContext(AuthContext);
 
     return (
         <div>
             <button onClick={toggleTheme}>Toggle the theme</button>
+            <br/>
+            {
+                isAuthenticated ? 'Hello User' : 'Please log in'
+            }
         </div>
     )
 }
